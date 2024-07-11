972faa8646230a9fed2d1bed4850053ac9d879ab
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2024-07-11 09:46:43 +0300

    Added a space after if

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ function checkAnswer(){
   let correctAnswer = "4";
   const userAnswer = document.querySelector('input[name=quiz]:checked').value;
   const feedback = document.querySelector("#feedback");
-  if(userAnswer === correctAnswer){
+  if (userAnswer === correctAnswer){
     feedback.textContent = "Correct! Well done.";
   } else {
     feedback.textContent = "That's incorrect. Try again!";
