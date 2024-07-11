83013b6c2437ff14e4e82d0e8ac5fb20fb423ff4
Andriamahazosoa Dimbinantenaina Rabearivony <arabearivony@aimsammi.org>
2024-07-11 09:14:40 +0300

    Quiz task: done

---
+++ quiz.js
@@ -0,0 +1,16 @@
+function checkAnswer(){
+  let correctAnswer = "4";
+  const userAnswer = document.querySelector('input[name=quiz]:checked').value;
+  const feedback = document.querySelector("#feedback");
+  if(userAnswer == correctAnswer){
+    feedback.textContent = "Correct ! Well done";
+  } else {
+    feedback.textContent = "That's incorrect. Try again!";
+  }
+}
+
+
+const submit = document.getElementById("submit-answer");
+submit.addEventListener("click", () => {
+  checkAnswer();
+} )
